b15caf1b93e081e035985b831806ffb825b25599
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-06-07 19:13:19 -0500

    feat: add StreakBar in landing screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,8 @@
-import { StreakDay } from "@/presentation/modules/streak/ui/components/StreakDay";
+import { LandingStackHeader } from "@/presentation/ui/components/LandingStackHeader";
 import { PageContainer } from "@/presentation/ui/components/PageContainer";
 import { PageHeader } from "@/presentation/ui/components/PageHeader";
 import { SectionHeader } from "@/presentation/ui/components/SectionHeader";
-import { Text } from "@/presentation/ui/components/Text";
-import { DARK_THEME } from "@/styles";
-import { Bell, Dumbbell, Flame } from "lucide-react-native";
-import { Pressable, View } from "react-native";
+import { StreakBar } from "@/presentation/ui/components/StreakBar";
 
 export default function Index() {
   return (
@@ -15,73 +12,7 @@ export default function Index() {
         description="Start achieving your fit goals"
       />
 
-      <View className="flex-row gap-2 items-center p-2 bg-fill-base rounded-2xl shadow-2xl shadow-fill-back/20">
-        <View className="relative size-14 aspect-square items-center justify-center bg-fill-middle rounded-xl border border-bd-muted">
-          <View className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Flame color={DARK_THEME.fgMuted} size={32} strokeWidth={1.5} />
-          </View>
-        </View>
-        <View className="flex-1 flex-row gap-1 pr-2">
-          <StreakDay text="M" completed={true} />
-          <StreakDay text="T" completed={true} />
-          <StreakDay text="W" completed={true} />
-          <StreakDay text="T" />
-          <StreakDay text="F" />
-          <StreakDay text="S" />
-          <StreakDay text="S" />
-        </View>
-      </View>
-
-      <View className="relative h-48 w-full bg-primary shadow-2xl shadow-fill-back/50 rounded-3xl p-4">
-        <Dumbbell
-          color={DARK_THEME.fgStrong}
-          strokeWidth={0.3}
-          size={196}
-          style={{
-            position: "absolute",
-            right: 0,
-            top: 0,
-            bottom: 0,
-            marginVertical: "auto",
-            opacity: 0.5,
-          }}
-        />
-
-        <View className="flex-1">
-          <View className="flex-row gap-4 items-center">
-            <View className="size-12 bg-primary-dark rounded-xl justify-center items-center">
-              <Dumbbell
-                color={DARK_THEME.fgStrong}
-                strokeWidth={1.5}
-                size={24}
-              />
-            </View>
-            <View>
-              <Text
-                font="funnel-display-600"
-                className="tracking-tight leading-none text-xl text-fg-strong"
-              >
-                Push Day
-              </Text>
-              <Text
-                font="funnel-display-300"
-                className="tracking-tight leading-none text-base text-fg-strong"
-              >
-                Tuesday, April 24
-              </Text>
-            </View>
-          </View>
-        </View>
-
-        <Pressable className="rounded-full h-12 bg-fill-top items-center justify-center w-auto">
-          <Text
-            font="outfit-500"
-            className="tracking-tight text-xl text-fg-strong w-auto"
-          >
-            {"Let's Train"}
-          </Text>
-        </Pressable>
-      </View>
+        <StreakBar className="w-fit self-center" />
 
       <View className="mt-4">
         <SectionHeader title="Your Sessions" />
--- src/presentation/modules/streak/ui/components/StreakDay.tsx
+++ src/presentation/modules/streak/ui/components/StreakDay.tsx
@@ -3,30 +3,28 @@ import { DARK_THEME } from "@/styles";
 import { Check, Circle } from "lucide-react-native";
 import { View } from "react-native";
 import { twMerge } from "tailwind-merge";
+import { withUniwind } from "uniwind";
 
 type Props = {
   completed?: boolean;
   text: string;
 };
 
+const IconCheck = withUniwind(Check);
+const IconCircle = withUniwind(Circle);
+
 export function StreakDay({ text, completed = false }: Props) {
   const color = completed ? DARK_THEME.fgStrong : DARK_THEME.fillMiddle;
   const className = completed ? "text-fg-default" : "text-fg-default";
   return (
-    <View
-      className={twMerge(
-        "justify-center gap-1 items-center p-1 rounded-3xl flex-1",
-      )}
-    >
-      <Text className={twMerge("w-8 text-center text-xs", className)}>
-        {text}
-      </Text>
+    <View className={twMerge("justify-center items-center rounded-3xl")}>
+      <Text className={twMerge("text-center text-sm", className)}>{text}</Text>
       {completed ? (
-        <View className="bg-linear-135 from-50% to-150% from-primary to-fill-base rounded-full size-7 justify-center items-center">
-          <Check color={color} strokeWidth={2} size={20} />
+        <View className="bg-primary rounded-full size-5 justify-center items-center">
+          <IconCheck strokeWidth={2} size={16} className="text-fg-strong" />
         </View>
       ) : (
-        <Circle fill={color} color={color} size={28} strokeWidth={1.5} />
+        <View className="bg-fill-middle size-5 rounded-full" />
       )}
     </View>
   );
