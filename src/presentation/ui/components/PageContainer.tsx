f2f72466c25ef621a71cba78a3c7e84cedb47967
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-06-07 19:15:16 -0500

    feat(PageContainer): add safe area insets

--- src/presentation/ui/components/PageContainer.tsx
+++ src/presentation/ui/components/PageContainer.tsx
@@ -1,14 +1,20 @@
 import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { twMerge } from "tailwind-merge";
 
 type Props = {
   children?: React.ReactNode;
   className?: string;
+  noHeader?: boolean;
 };
 
-export function PageContainer({ children, className }: Props) {
+export function PageContainer({ children, className, noHeader }: Props) {
+  const insets = useSafeAreaInsets();
   return (
-    <View className={twMerge("p-6 pt-12 size-full gap-4", className)}>
+    <View
+      className={twMerge("px-2 size-full gap-4", className)}
+      style={{ paddingBottom: insets.bottom, paddingTop: insets.top }}
+    >
       {children}
     </View>
   );
