852dace7dfc05810cfa895039763e573d7f68d5b
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-06-11 00:25:49 -0500

    refactor(CustomTabBar): improve styles

--- src/presentation/modules/tabs/ui/components/CustomTabBar.tsx
+++ src/presentation/modules/tabs/ui/components/CustomTabBar.tsx
@@ -8,6 +8,7 @@ import {
 import { CustomTab } from "@/presentation/modules/tabs/ui/components/CustomTab";
 import { BottomTabBarProps } from "expo-router/build/react-navigation/bottom-tabs";
 import { View } from "react-native";
+import { twMerge } from "tailwind-merge";
 
 type Props = {
   className?: string;
@@ -31,41 +32,46 @@ export function CustomTabBar({
 }: Props & BottomTabBarProps) {
   return (
     <View
-      className="absolute bottom-0 left-2 right-2 h-16 rounded-full bg-fill-middle flex-row items-center justify-center px-2"
+      className={twMerge(
+        "absolute shrink-0 w-auto bottom-0 left-0 right-0 h-16",
+        className,
+      )}
       style={{ marginBottom: insets.bottom }}
     >
-      {state.routes.map((r, i) => {
-        const focused = state.index === i;
+      <View className="self-center shadow-2xl shadow-fill-back-dark border border-bd-muted shrink-0 rounded-full bg-fill-middle flex-row items-center justify-center px-2">
+        {state.routes.map((r, i) => {
+          const focused = state.index === i;
 
-        const options = descriptors[r.key].options;
+          const options = descriptors[r.key].options;
 
-        const label =
-          typeof options.tabBarLabel === "string"
-            ? options.tabBarLabel
-            : (options.title ?? r.name);
+          const label =
+            typeof options.tabBarLabel === "string"
+              ? options.tabBarLabel
+              : (options.title ?? r.name);
 
-        const onPress = () => {
-          const event = navigation.emit({
-            target: r.key,
-            type: "tabPress",
-            canPreventDefault: true,
-          });
+          const onPress = () => {
+            const event = navigation.emit({
+              target: r.key,
+              type: "tabPress",
+              canPreventDefault: true,
+            });
 
-          if (!focused && !event.defaultPrevented) {
-            navigation.navigate(r.name, r.params);
-          }
-        };
+            if (!focused && !event.defaultPrevented) {
+              navigation.navigate(r.name, r.params);
+            }
+          };
 
-        return (
-          <CustomTab
-            key={r.key}
-            Icon={icons[r.name as never] ?? IconHome}
-            label={label}
-            active={focused}
-            onPress={onPress}
-          />
-        );
-      })}
+          return (
+            <CustomTab
+              key={r.key}
+              Icon={icons[r.name as never] ?? IconHome}
+              label={label}
+              active={focused}
+              onPress={onPress}
+            />
+          );
+        })}
+      </View>
     </View>
   );
 }
