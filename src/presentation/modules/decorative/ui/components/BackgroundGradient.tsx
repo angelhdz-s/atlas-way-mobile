1ff75b9d86d5b0312d601049635c547ab6fde44e
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-06-11 00:24:21 -0500

    feat: add BackgroundGradient component

---
+++ src/presentation/modules/decorative/ui/components/BackgroundGradient.tsx
@@ -0,0 +1,17 @@
+import { View } from "react-native";
+import { twMerge } from "tailwind-merge";
+
+type Props = {
+  className?: string;
+};
+
+export function BackgroundGradient({ className }: Props) {
+  return (
+    <View
+      className={twMerge(
+        "z-0 absolute w-[110%] -left-5 -top-20 h-90 bg-linear-350 from-50% to-100% from-fill-back to-fill-base",
+        className,
+      )}
+    />
+  );
+}
