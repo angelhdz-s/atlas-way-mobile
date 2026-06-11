6723a8da1e507f5dff17c5b9572a4c1268870f27
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-06-11 04:24:00 -0500

    refactor(FilterBar): update placeholder

--- src/presentation/ui/components/FilterBar.tsx
+++ src/presentation/ui/components/FilterBar.tsx
@@ -1,10 +1,10 @@
 import {
   IconSearch,
   IconSlidersHorizontal,
-} from "@/presentation/modules/icon/ui/components/Icons";
-import { DARK_THEME } from "@/styles";
-import { TextInput, View } from "react-native";
-import { twMerge } from "tailwind-merge";
+} from '@/presentation/modules/icon/ui/components/Icons';
+import { DARK_THEME } from '@/styles';
+import { TextInput, View } from 'react-native';
+import { twMerge } from 'tailwind-merge';
 
 type Props = {
   className?: string;
@@ -12,11 +12,11 @@ type Props = {
 
 export function FilterBar({ className }: Props) {
   return (
-    <View className={twMerge("flex-row items-center gap-4", className)}>
+    <View className={twMerge('flex-row items-center gap-4', className)}>
       <View className="px-2 flex-row items-center gap-2 bg-fill-middle flex-1 rounded-lg">
         <IconSearch className="text-fg-muted" size={16} strokeWidth={1.5} />
         <TextInput
-          placeholder="... Search"
+          placeholder="Search..."
           placeholderTextColor={DARK_THEME.fgMuted}
           className="text-fg-default min-h-10 flex-1"
         />
