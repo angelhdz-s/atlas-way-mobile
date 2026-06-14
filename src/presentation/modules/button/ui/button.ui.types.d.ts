d2dca05e61fcee3ae7824ab431f8da0757e96e95
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-06-14 16:56:26 -0500

    feat: add variant button constants

---
+++ src/presentation/modules/button/ui/button.ui.types.d.ts
@@ -0,0 +1,35 @@
+import type { PressableProps } from 'react-native';
+import type { ButtonVariantProps } from '@/presentation/modules/button/ui/button.ui.variants';
+
+export type ButtonVariantsType = {
+  color: {
+    primary: string;
+    outline: string;
+    subtle: string;
+    simple: string;
+  };
+  size: {
+    sm: string;
+    md: string;
+    lg: string;
+  };
+};
+
+type ButtonColorVariant = Pick<ButtonVariantsType, 'color'>;
+type ButtonSizeVariant = Pick<ButtonVariantsType, 'size'>;
+
+export type ButtonDefaultColorVariantKey = Record<
+  keyof ButtonColorVariant,
+  keyof ButtonVariantsType['color']
+>;
+export type ButtonSizeVariantKey = Record<
+  keyof ButtonSizeVariant,
+  keyof ButtonVariantsType['size']
+>;
+
+export type ButtonDefaultVariants = ButtonDefaultColorVariantKey &
+  ButtonSizeVariantKey;
+
+export type ButtonProps = PressableProps & {
+  variant: ButtonVariantProps;
+};
