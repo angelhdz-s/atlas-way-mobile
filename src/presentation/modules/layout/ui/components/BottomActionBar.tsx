3ef82148643ffdc2d90f6127a38539f559a5ca30
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-06-14 17:09:28 -0500

    feat: add BottomActionBar component

---
+++ src/presentation/modules/layout/ui/components/BottomActionBar.tsx
@@ -0,0 +1,24 @@
+import { twMerge } from 'tailwind-merge';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { View } from 'react-native';
+import { TAB_BAR_HEIGHT } from '@/presentation/modules/tabs/ui/tabs.ui.constants';
+import { getTarBarMarginBottom } from '@/presentation/modules/tabs/ui/tabs.ui.utils';
+
+type Props = {
+  className?: string;
+  children?: React.ReactNode;
+};
+
+export function BottomActionBar({ className, children }: Props) {
+  const insets = useSafeAreaInsets();
+  return (
+    <View
+      className={twMerge('absolute left-4 right-4 z-1', className)}
+      style={{
+        bottom: getTarBarMarginBottom(insets.bottom) + TAB_BAR_HEIGHT + 16,
+      }}
+    >
+      {children}
+    </View>
+  );
+}
