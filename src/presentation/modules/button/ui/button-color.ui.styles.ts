794aa79dae111197d65f46cf6769c282a5e76ca5
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-06-16 04:10:43 -0500

    feat: button color ui style utility

--- src/presentation/modules/button/ui/button-color.ui.styles.ts
+++ src/presentation/modules/button/ui/button-color.ui.styles.ts
@@ -1,8 +1,9 @@
 import type { ButtonColorVariant } from '@/presentation/modules/button/ui/button.ui.types';
+import { BUTTON_DEFAULT_VARIANTS } from '@/presentation/modules/button/ui/button.ui.variants.constants';
 import { DARK_THEME, LIGHT_THEME } from '@/styles';
 
-export function getButtonTextStyles(
-  colorVariant?: keyof ButtonColorVariant['color']
+export function getButtonColors(
+  colorVariant: keyof ButtonColorVariant['color'] = BUTTON_DEFAULT_VARIANTS.color
 ) {
   const darkColor = DARK_THEME.fgStrong;
   const lightColor = LIGHT_THEME.fgStrong;
