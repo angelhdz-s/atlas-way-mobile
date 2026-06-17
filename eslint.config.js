2c248c96df91af92add6657eb545f0bd73d6c0fa
angelhdz-s <angelhdzsotelo15@gmail.com>
2026-06-17 00:29:13 -0500

    chore: add eslint unused imports plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ const expoConfig = require('eslint-config-expo/flat');
 const jsEslint = require('@eslint/js');
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 const typescriptEslint = require('typescript-eslint');
+const unusedImports = require('eslint-plugin-unused-imports');
 
 module.exports = defineConfig([
   {
@@ -13,8 +14,13 @@ module.exports = defineConfig([
       jsEslint.configs.recommended,
       typescriptEslint.configs.recommended,
     ],
+    plugins: {
+      'unused-imports': unusedImports,
+    },
     ignores: ['dist/*'],
     rules: {
+      'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+      'unused-imports/no-unused-imports': 'error',
       'prettier/prettier': 'off',
       '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
